92f9f831bd7e2424f1e80ad41c1b5a0c728100e0
magmag6240 <mayizuojing780@gmail.com>
2024-07-17 21:02:58 +0900

    コントローラーの更新

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
